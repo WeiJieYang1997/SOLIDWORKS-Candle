b8c601230cd849ee2ee58ea6de7b457e075634e2
WeiJIe Yang <38777825+WeiJieYang1997@users.noreply.github.com>
2020-02-14 01:54:26 -0500

    Add files via upload

---
+++ Cut/SolidWorksMacro.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+
+namespace Cut
+{
+    public partial class SolidWorksMacro
+    {
+        public void Main()
+        {
+            //ADD THESE LINES OF CODE
+            double holeRadius;
+            double holeDepth;
+            //Create an instance of the user form
+
+            frmCutExtrude myForm = new frmCutExtrude();
+
+            //Set the title for the form
+            myForm.Text = "Size of Cut-Extrude in Millimeters";
+            //Display the user form and retrieve radius and 
+            //depth values typed by the user; divide those values
+            //by 1000 to change millimeters to meters
+            myForm.ShowDialog();
+            holeRadius = myForm.radius / 1000;
+            holeDepth = myForm.depth / 1000;
+            //Dispose of the user form and remove it from
+            //memory because it's no longer needed
+            myForm.Dispose();
+
+
+            ModelDoc2 swDoc = null;
+            
+            //bool boolstatus = false;
+            //var COSMOSWORKSObj = null;
+            swDoc = ((ModelDoc2)(swApp.ActiveDoc));
+            //COSMOSWORKSObj COSMOSWORKSObj = null;
+            //CWAddinCallBackObj CWAddinCallBackObj = null;
+            //CWAddinCallBackObj = swApp.GetAddInObject("CosmosWorks.CosmosWorks");
+            //COSMOSWORKSObj = CWAddinCallBackObj.COSMOSWORKS;
+            ModelView myModelView = null;
+            myModelView = ((ModelView)(swDoc.ActiveView));
+            myModelView.FrameState = ((int)(swWindowState_e.swWindowMaximized));
+            //boolstatus = swDoc.Extension.SelectByRay(0.00053592862681739462, 0.2199999999999136, -0.0045118054006820785, -0.18724816575405859, -0.66456615869841951, -0.723387824845406, 0.0010862903128070283, 2, false, 0, 0);
+            //ADD THESE LINES OF CODE 
+            //Get coordinates of selection point
+            SelectionMgr swSelectionMgr = null;
+            swSelectionMgr = (SelectionMgr)swDoc.SelectionManager;
+            double[] SelectCoordinates;
+            SelectCoordinates = (double[])swSelectionMgr.GetSelectionPoint2(1, -1);
+            //If face is selected, then open a sketch;
+            //otherwise, stop execution
+            object SelectedObject = null;
+            SelectedObject = (object)swSelectionMgr.GetSelectedObject6(1, 0);
+            int objtype;
+            objtype = (int)swSelectionMgr.GetSelectedObjectType3(1, -1);
+            if (objtype == (int)swSelectType_e.swSelFACES)
+            {
+                swDoc.SketchManager.InsertSketch(true);
+            }
+
+            //swDoc.ClearSelection2(true);
+            SketchSegment skSegment = null;
+            skSegment = ((SketchSegment)(swDoc.SketchManager.CreateCircle(0, 0, 0, 0.010505, -0.007509, 0)));
+            Feature myFeature = null;
+            myFeature = ((Feature)(swDoc.FeatureManager.FeatureCut4(true, false, false, 0, 0, 0.02, 0.01, true, false, false, false, 0.26179938779914946, 0.017453292519943334, false, false, false, false, false, true, true, true, true, false, 0, 0, false, false)));
+            swDoc.ISelectionManager.EnableContourSelection = false;
+            //StudyManagerObj = null;
+            //ActiveDocObj = null;
+            //CWAddinCallBackObj = null;
+            //COSMOSWORKSObj = null;
+
+
+            //ADD THESE LINES OF CODE 
+            //Get IMathPoint to use when transforming
+            //from model space to sketch space
+            MathUtility swMathUtility = null;
+            MathPoint swMathPoint = null;
+            Sketch swSketch = null;
+            double dx;
+            double dy;
+            double dz;
+            swMathUtility = (MathUtility)swApp.GetMathUtility();
+            swMathPoint = (MathPoint)swMathUtility.CreatePoint(SelectCoordinates);
+            //Get reference to sketch
+            swSketch = (Sketch)swDoc.SketchManager.ActiveSketch;
+            //Translate sketch point into sketch space
+            MathTransform swMathTransform = null;
+            swMathTransform = (MathTransform)swSketch.ModelToSketchTransform;
+            swMathPoint = (MathPoint)swMathPoint.MultiplyTransform(swMathTransform);
+            //Retrieve coordinates of the sketch point
+            double[] darray;
+            darray = (double[])swMathPoint.ArrayData;
+            dx = darray[0];
+            dy = darray[1];
+            dz = darray[2];
+            //Use swDoc.SketchManager.CreateCircleByRadius instead of
+            //swDoc.SketchManager.CreateCircle because
+            //swDoc.SketchManager.CreateCircleByRadius sketches a
+            //circle centered on a sketch point and lets you 
+            //specify a radius
+            double radius = 0.015;
+            SketchSegment swSketchSegment = null;
+            swSketchSegment = (SketchSegment)swDoc.SketchManager.CreateCircleByRadius(dx, dy, dz, radius);
+            //Create the cut extrude feature
+            Feature swFeature = null;
+            swFeature = (Feature)swDoc.FeatureManager.FeatureCut3(true, false, false, 0, 0, 0.025, 0.01, true, false, false, false, 0, 0, false, false, false, false, false, true, true, false, false, false, (int)swStartConditions_e.swStartSketchPlane, 0, false);
+        }
+
+
+        // The SldWorks swApp variable is pre-assigned for you.
+        public SldWorks swApp;
+
+    }
+}
+
---
+++ Cut/frmCutExtrude.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Cut
+{
+    partial class frmCutExtrude
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtDepth = new System.Windows.Forms.TextBox();
+            this.bOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(172, 42);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 0;
+            // 
+            // txtDepth
+            // 
+            this.txtDepth.Location = new System.Drawing.Point(172, 117);
+            this.txtDepth.Name = "txtDepth";
+            this.txtDepth.Size = new System.Drawing.Size(100, 20);
+            this.txtDepth.TabIndex = 1;
+            // 
+            // bOK
+            // 
+            this.bOK.Location = new System.Drawing.Point(172, 196);
+            this.bOK.Name = "bOK";
+            this.bOK.Size = new System.Drawing.Size(75, 23);
+            this.bOK.TabIndex = 2;
+            this.bOK.Text = "OK";
+            this.bOK.UseVisualStyleBackColor = true;
+            this.bOK.Click += new System.EventHandler(this.bOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Radius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Depth";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bOK);
+            this.Controls.Add(this.txtDepth);
+            this.Controls.Add(this.txtRadius);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtDepth;
+        private System.Windows.Forms.Button bOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
